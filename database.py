037e142b486d5f2e10d0896a9db1e1be26fad333
hwhhemm <159470517+epltv1@users.noreply.github.com>
2025-08-18 19:00:20 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,5 @@
 # database.py
 import sqlite3
-from datetime import datetime
 
 class Database:
     def __init__(self):
@@ -16,6 +15,8 @@ class Database:
                 rtmp_url TEXT,
                 stream_key TEXT,
                 stream_title TEXT,
+                logo_url TEXT,
+                text_overlay TEXT,
                 start_time TIMESTAMP
             )
         ''')
@@ -26,12 +27,12 @@ class Database:
         ''')
         self.conn.commit()
 
-    def add_stream(self, stream_id, m3u8_link, rtmp_url, stream_key, stream_title):
+    def add_stream(self, stream_id, m3u8_link, rtmp_url, stream_key, stream_title, logo_url, text_overlay):
         cursor = self.conn.cursor()
         cursor.execute('''
-            INSERT INTO streams (stream_id, m3u8_link, rtmp_url, stream_key, stream_title, start_time)
-            VALUES (?, ?, ?, ?, ?, ?)
-        ''', (stream_id, m3u8_link, rtmp_url, stream_key, stream_title, datetime.utcnow()))
+            INSERT INTO streams (stream_id, m3u8_link, rtmp_url, stream_key, stream_title, logo_url, text_overlay, start_time)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (stream_id, m3u8_link, rtmp_url, stream_key, stream_title, logo_url, text_overlay, datetime.utcnow()))
         self.conn.commit()
 
     def get_all_streams(self):
