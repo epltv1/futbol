98d7ab98ef4f9c9ae4de3e93386fe3d1d91e0f32
hwhhemm <159470517+epltv1@users.noreply.github.com>
2025-09-11 21:23:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # config.py
-BOT_TOKEN = "7454188408:AAHIsg6i73RDj_hiu2gc5HRYeZ_OMOD68FU"  # Replace with your Telegram bot token
+BOT_TOKEN = "7670073769:AAFytJ2lrsX_JmRNvwEQbTjCptbg6oDr69Q"  # Replace with your Telegram bot token
 OWNER_ID = 1882002437   # Replace with your Telegram ID (e.g., 123456789)
